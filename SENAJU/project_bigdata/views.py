99c6c8162c5da41db5f0bf41465b2aa4786c778d
MEGAZONE ClOUD <blackcode-@naver.com>
2022-10-12 15:43:46 +0900

    .

--- SENAJU/project_bigdata/views.py
+++ SENAJU/project_bigdata/views.py
@@ -33,7 +33,7 @@ import time
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from datetime import datetime
+# from datetime import datetime
 import random
 
 
